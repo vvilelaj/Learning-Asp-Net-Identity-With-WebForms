61a7f7e9683f74a90daee3ce7756c124abc0a7c6
Victor Vilela <vilela_victor@hotmail.com>
2017-06-18 16:47:11 -0500

    Add OWIN autentication
    Add Host.SystemWeb nuget package.
    Add Identity.Owin nuget package.
    Change Register.aspx to singin the user.

--- VVJ.LeargingAspNetIdentity.WebForms/Register.aspx.cs
+++ VVJ.LeargingAspNetIdentity.WebForms/Register.aspx.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace VVJ.LearningAspNetIdentity.WebForms
 {
@@ -20,13 +18,16 @@ namespace VVJ.LearningAspNetIdentity.WebForms
         {
             var userStore = new UserStore<IdentityUser>();
             var manager = new UserManager<IdentityUser>(userStore);
-
             var user = new IdentityUser() { UserName = UserName.Text };
+
             var result = manager.Create(user, Password.Text);
 
             if (result.Succeeded)
             {
-                StatusMessage.Text = $"User {user.UserName} was created succefully!";
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties(){ },  userIdentity);
+                Response.Redirect("~/Login.aspx");
             }
             else
             {
