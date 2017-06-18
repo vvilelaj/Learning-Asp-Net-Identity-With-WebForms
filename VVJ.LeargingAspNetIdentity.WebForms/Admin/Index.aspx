eb3e55931823786745dadcd52c9700215cc858b4
Victor Vilela <vilela_victor@hotmail.com>
2017-06-18 17:30:30 -0500

    Add Login feature.
    Change web.Config to support forms authenticatiosn.
    Add web.config to Admin folder to restrict anonymous users.

---
+++ VVJ.LeargingAspNetIdentity.WebForms/Admin/Index.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="VVJ.LearningAspNetIdentity.WebForms.Admin.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="font-family: Arial, Helvetica, sans-serif; font-size: small">
+    <form id="form1" runat="server">
+        <div>
+            <h4>Admin Index</h4>
+        </div>
+    </form>
+</body>
+</html>
---
+++ VVJ.LeargingAspNetIdentity.WebForms/Login.aspx.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VVJ.LearningAspNetIdentity.WebForms
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (User.Identity.IsAuthenticated)
+                {
+                    StatusText.Text = $"Hello {User.Identity.Name}!!";
+                    LoginStatus.Visible = true;
+                    LogoutButton.Visible = true;
+                }
+                else
+                {
+                    LoginForm.Visible = true;
+                }
+            }
+        }
+
+        protected void SignIn(object sender, EventArgs e)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var manager = new UserManager<IdentityUser>(userStore);
+            var user = manager.Find(UserName.Text, Password.Text);
+            if (user != null)
+            {
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties { IsPersistent = true }, userIdentity);
+
+                var returnUrl = (string.IsNullOrWhiteSpace(Request.QueryString["ReturnUrl"])) ? "~/Login.aspx" : Request.QueryString["ReturnUrl"];
+                Response.Redirect(returnUrl);
+            }
+            else
+            {
+                StatusText.Text = "Invalid username or password.";
+                LoginStatus.Visible = true;
+            }
+        }
+
+        protected void SignOut(object sender, EventArgs e)
+        {
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            authenticationManager.SignOut();
+            Response.Redirect("~/Login.aspx");
+        }
+    }
+}
